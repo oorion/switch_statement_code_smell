6ab841adac42616829e02c76ca8c777bbf0d6e0f
Orion Osborn <orion@MacBook-Air-22.local>
2017-05-01 20:18:42 -0600

    initial class

---
+++ employee.rb
@@ -0,0 +1,36 @@
+class Employee
+  Engineer = 0
+  Salesman = 1
+  Manager = 2
+
+  attr_reader :type
+
+  def initialize(type)
+    @type = type
+  end
+
+  def pay_amount
+    case type
+    when Engineer
+      monthly_salary
+    when Salesman
+      monthly_salary + commission
+    when Manager
+      monthly salary + bonus
+    end
+  end
+
+  private
+
+  def monthly_salary
+    10
+  end
+
+  def commission
+    4
+  end
+
+  def bonus
+    2
+  end
+end
